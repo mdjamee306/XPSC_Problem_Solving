3b920bace33dd08983b4dc7641bea3f9ac279d22
jaamee_0.0_ <138340983+mdjamee306@users.noreply.github.com>
2024-11-01 23:27:36 +0600

    Add files via upload
    added week 1 day 6

---
+++ Week 1/Day 6/G_-_How_many.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int S, T;
+    cin >> S >> T;
+    int count = 0;
+    for (int a = 0; a <= S; a++)
+    {
+        for (int b = 0; b <= S - a; b++)
+        {
+            for (int c = 0; c <= S - a - b; c++)
+            {
+                if (a * b * c <= T)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
